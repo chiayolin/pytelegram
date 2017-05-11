23e926c682c0d47c1e83292d190dd434dc6eeba0
chiayolin <chiayo.lin@gmail.com>
2017-05-11 11:54:26 +0700

    extract telegram.py from my CSIA

---
+++ telegram.py
@@ -0,0 +1,38 @@
+import requests
+
+class Telegram: 
+
+    def __init__(self, token):
+        self.token = token
+    
+    def __telegram(self, token, method, payload = {}):
+        return requests.get('https://api.telegram.org/bot' + \
+                self.token + method, params = payload).json()
+    
+    def getMe(self):
+        return self.__telegram(self.token, '/getMe')
+
+    def getUpdates(self, offset, timeout = 60):
+        return self.__telegram(self.token, '/getUpdates',
+                {'timeout' : timeout, 'offset' : offset})
+        
+    def sendMessage(self, recipient, message, parse_mode = 'HTML'):
+        return self.__telegram(self.token, '/sendMessage', 
+                {'chat_id' : recipient, 'text' : message, 
+                    'parse_mode' : parse_mode})
+
+    def listen(self, callback, parse_mode = 'HTML', offset = 0):
+        while True:
+            request = self.getUpdates(offset)
+            if not (request['ok'] and request['result']): continue
+
+            for result in request['result']:
+                message = result['message']['text'] 
+                chat_id = result['message']['chat']['id']
+
+                response = callback({'self' : self, 'message' : message, 
+                    'chat_id' : chat_id}, response = '')
+
+                self.sendMessage(chat_id, response, parse_mode)
+
+                offset = result['update_id'] + 1
