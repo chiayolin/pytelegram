801d372664a064ad8bc702c583edef960ebb249a
chiayolin <chiayo.lin@gmail.com>
2017-05-11 12:10:58 +0700

    minimalize function arguments

--- telegram.py
+++ telegram.py
@@ -30,8 +30,8 @@ class Telegram:
                 message = result['message']['text'] 
                 chat_id = result['message']['chat']['id']
 
-                response = callback({'self' : self, 'message' : message, 
-                    'chat_id' : chat_id}, response = '')
+                response = callback({'self' : self,
+                    'message' : message, 'chat_id' : chat_id})
 
                 self.sendMessage(chat_id, response, parse_mode)
 
